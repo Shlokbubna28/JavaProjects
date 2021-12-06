bc7ded103a47f666ea4cc84122d2ee6dcd882844
Shlokbubna28 <95333733+Shlokbubna28@users.noreply.github.com>
2021-12-06 16:41:44 +0530

    Created hello.java
    This specific program prints "Hello!", on the output screen.
    The package I have created was for me and so in order to print this program and get the correct output without any syntax error, The user has to either create a same named package "Hellojava", on their programming platform or simply remove the package statement from my program. Then compile it and they will get the correct output, "Hello!".
    Thank you!

---
+++ hello.java
@@ -0,0 +1,8 @@
+package Hellojava;                         // a package containing the class "Hello".
+public class Hello                 // class created 
+{
+  public static void main(String args[])           // void method 
+  {
+    System.out.println("Hello!");                  // Prints hello on the terminal screen
+  }
+}
