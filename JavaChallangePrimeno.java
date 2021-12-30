0654a7a8753760f3035e00b92d2cd00e3a5bf80b
Shlok Bubna <95333733+Shlokbubna28@users.noreply.github.com>
2021-12-31 03:59:41 +0530

    JavaChallangePrimeno.java
    This program checks whether the given number is a prime number or not.

---
+++ JavaChallangePrimeno.java
@@ -0,0 +1,39 @@
+
+import java.util.Scanner;
+public class JavaChallangePrimeno
+{
+    public static void main(String args[])
+    {
+       Scanner sc = new Scanner(System.in);
+
+       // FIRST ENTER A VALUE, THEN TAKE IT AS AN INPU
+
+        System.out.println("Enter a number: ");    //Taking number input
+        int num = sc.nextInt();
+
+       int i, m=0,flag =0;
+       m = num/2;
+
+       if(num ==0 || num ==1)
+       {
+           System.out.println("It is not a prime number");
+       }
+       else
+       {
+           for(i=2; i<=m; i++)
+           {
+               if(num%i==0)
+               {
+                   System.out.println("It is not a prime number");
+                   flag = 1;
+                   break;
+               }
+           }
+           if(flag ==0)
+           {
+               System.out.println("It is a prime number ");
+           }
+       }
+
+    }
+}
